d6e3ee67b02c6019222ea039adfabb26d9db1771
irons163 <irons@ironsdeMacBook-Air.local>
2015-06-27 17:47:59 +0800

    Initial Commit

---
+++ Try_Number_Travel/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Try_Number_Travel
+//
+
+//  Copyright (c) 2015年 irons. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
